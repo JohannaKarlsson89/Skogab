fe92934656a2c276d4c40271abbc36b462e2135d
Johanna Karlsson <joka2113@student.miun.se>
2022-04-12 19:27:30 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu')
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('is-active');
+    mobileMenu.classList.toggle('is-active');
+});
